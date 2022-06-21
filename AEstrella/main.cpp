63ecba9eaa2630f6d611c6793608ae34746e9e3a
Freddyp666 <freddy-pat@hotmail.es>
2022-06-21 17:13:06 -0500

    Update main.cpp
    s

--- AEstrella/main.cpp
+++ AEstrella/main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     cout << "Hello" << endl;
     cout<<"Prueba Chris"<<endl;
-    cout << "Hello world!Prueba_Freddy " << endl;
+    cout << "Hello world!Prueba_Freddy  " << endl;
     return 0;
 }
